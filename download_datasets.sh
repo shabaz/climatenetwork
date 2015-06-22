7dde6bc8a464fe00fd03e28cda3c4aee8d6226de
Victor Azizi <azizivictor@gmail.com>
2015-06-22 16:29:12 +0200

    edited for multithreading

--- download_datasets.sh
+++ download_datasets.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
-wget ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.derived/surface/air.mon.mean.nc
-wget ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.derived/surface/pr_wtr.mon.mean.nc
-wget ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.derived/surface/pres.mon.mean.nc
-wget ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.derived/surface/rhum.mon.mean.nc
+#/bin/sh
+wget
+ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.derived/surface/air.mon.mean.nc &
+wget ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.derived/surface/pr_wtr.mon.mean.nc &
+wget ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.derived/surface/pres.mon.mean.nc &
+wget ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.derived/surface/rhum.mon.mean.nc &
