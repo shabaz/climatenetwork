6bd30af8cc632c8d5eed8535931824aae173729d
Shabaz Sultan <shabaz.sultan@gmail.com>
2015-06-22 13:40:47 +0200

    download script

---
+++ download_datasets.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+wget ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.derived/surface/air.mon.mean.nc
+wget ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.derived/surface/pr_wtr.mon.mean.nc
+wget ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.derived/surface/pres.mon.mean.nc
+wget ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.derived/surface/rhum.mon.mean.nc
