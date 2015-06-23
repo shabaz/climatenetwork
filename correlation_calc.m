563a70b2a807b8f81e5d79749db83c3f37c80218
Victor Azizi <azizivictor@gmail.com>
2015-06-23 15:30:31 +0200

    adjusted correlation_calc to be correct

--- correlation_calc.m
+++ correlation_calc.m
@@ -5,10 +5,10 @@ for window=0:11,
     results = zeros(10512, 10512);
     tic;
 
-    for offset=-6:6,
+    %for offset=-6:6,
         % select subset for current window and offset
-        X = reshape(A(:,(1:60)+6+window*60), 10512,60)';
-        Y = reshape(B(:,(1:60)+offset+6+window*60), 10512,60)';
+        X = reshape(A(:,(1:60)+window*60), 10512,60)';
+        Y = reshape(B(:,(1:60)+window*60), 10512,60)';
         %normalize dataset by subtracting means
         Xn=bsxfun(@minus,X,mean(X,1)); 
         Yn=bsxfun(@minus,Y,mean(Y,1));
@@ -21,7 +21,7 @@ for window=0:11,
         res = res ./ st;
         res = abs(res);
         results = max(res, results);
-    end;
+    %end;
     %store using 8 bit numbers, abs corr.coef. is only in 0-1 range and a
     %resolution of 0-255 seems good enough to capture it (vs 64 bit double)
     results = uint16(results*65025);
