276ecd119d09f60a0bd31970783a7dbc8d536fbe
Victor Azizi <azizivictor@gmail.com>
2015-06-25 11:05:41 +0200

    hub calc and change_commies updated

--- change_commies.py
+++ change_commies.py
@@ -1,3 +1,14 @@
+max_len = 0
+with open('communities.txt', 'r') as f:
+    f.readline()
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        line_len = len(line.split()) - 3
+        if line_len > max_len:
+            max_len=line_len
+
 with open('communities.txt', 'r') as f:
     f.readline()
     while True:
@@ -5,4 +16,10 @@ with open('communities.txt', 'r') as f:
         if not line:
             break
         line = line.rstrip('\n')
-        print(''.join(line.split()[3:]).rstrip('}').lstrip('{').replace(',',' '))
+        line = line.split()[3:]
+        line[0] = line[0].lstrip('{')
+        line[-1] = line[-1].rstrip('}')
+
+        for i in range(len(line), max_len):
+            line.append(',0')
+        print(''.join(line).replace(',',' '))
--- hub_calc.m
+++ hub_calc.m
@@ -1,3 +1,17 @@
 function c_post = hub_calc(n)
-c_post = n;
+    %We still have to call the python program here
+
+    %open and iterate
+    commies = load('real_communities.txt');
+
+    c_post = zeros(size(n,2),size(commies, 1));
+
+    for i = 1:size(commies,1),
+        for j = 1:size(commies,2),
+            t = commies(i,j);
+            if t,
+                c_post(t, i) = 1;
+            end
+        end
+    end
 end
