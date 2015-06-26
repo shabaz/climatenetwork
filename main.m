2275ca593ba9c333a44890c52b5db10b2d5f9c91
Victor Azizi <azizivictor@gmail.com>
2015-06-26 12:56:44 +0200

    working prediction in main

--- main.m
+++ main.m
@@ -32,13 +32,15 @@ if exist(file_name, 'file') == 0
   correlation_calc(want, given, want_name, given_name);  
 end
 
+%c_pre = granger_calc(want, given);
 %c_pre should be a {data_type_we_want x data_type_we_predict_from}
 c_pre = importdata(file_name);
 n = network_calc(c_pre);
 %c_post should be a {datapoint (date we predict from) x hub} matrix
 c_post = hub_calc(n);
+
 n_points = 12;
-c_post = rand(10512, 4)/10512;
+%c_post = rand(10512, 4)/10512;
 %p should be a {datapoint x predictions} matrix
 p = create_predictions(c_post, given, n_points);
 
